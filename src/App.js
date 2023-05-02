e4926110adfc93449e0a04ef542bd5d917cd0cf2
KrisKipper <yarevyakinakristina@gmail.com>
2023-05-02 16:45:27 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './styles/styles.css';
+import Button from "./components/button/button";
 
 function App() {
   return (
     <div className="App">
+      <Button size="small" label="Маленькая кнопка" />
+      <Button size="large" label="Большая кнопка" />
     </div>
   );
 }
